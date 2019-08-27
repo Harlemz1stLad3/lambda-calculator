45e82c2f91f24f7e6b7d2998cbcf7aa892c19470
Juanita Purcell <brittanyhugz@gmail.com>
2019-08-27 11:16:12 -0400

    trying to get this on my screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React, { useState } from "react";
-import "./App.scss";
+import React from "react";
+import "./App.css";
 // STEP 4 - import the button and display components
-import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"
-import Operators from "./components/ButtonComponents/OperatorButtons/Operators.js"
-import Specials from "./components/ButtonComponents/SpecialButtons/Specials.js"
-
 // Don't forget to import any extra css/scss files you build into the correct component
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers"; 
+
+import Operators from "./components/ButtonComponents/OperatorButtons/Operators"; 
+
+import Specials from "./components/ButtonComponents/SpecialButtons/Specials"; 
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
-import Display from './components/DisplayComponents/Display.js'
+
+import Display from "./components/DisplayComponents/Display";
+
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -18,57 +21,23 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
-  const [displayValue, setDisplayValue] = useState(" ")
-
-  const addNumber = (number) => {
-    setDisplayValue(displayValue => displayValue + number) 
-  }
-
-  const addOperator = (operator) => {
-    if (operator === '='){
-      setDisplayValue(displayValue => eval(displayValue));
-    }else{
-      setDisplayValue(displayValue => displayValue + " " + operator + " ")
-    }
-  }
-
-  const addSpecial = (special) => {
-    console.log(special);
-    if (special === "C"){ 
-      setDisplayValue(" ")
-    }
-
-
-    if (special === "+/-"){
-      if (displayValue > 0){
-        setDisplayValue(-Math.abs(displayValue))}
-    }  
-      if (displayValue < 0){
-        setDisplayValue(Math.abs(displayValue))
-    }
-
-
-    if (special === "%"){
-      setDisplayValue(displayValue / 100)
-    }
-  }
-
   return (
     <div className="container">
       <Logo />
+      <Display />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-      <Display number = {displayValue} />
-      <Numbers addNumber = {addNumber}/>
-      <Operators addOperator={addOperator}/>
-      <Specials addSpecial = {addSpecial}/>
+        {/* STEP 4 - Render your components here and be sure to properly import/export all files */
+        }
+        <div className ="flex">
+          <div className = "flex column">
+            <Specials />
+            <Numbers />
+          </div>
+          <Operators />
+        </div>
       </div>
     </div>
   );
 }
- 
+
 export default App;
-
-//known bugs - if you prefix a number with 0 when trying to perform an operation, it will throw an error.scss
-//ex - 085.9 + 2856 = ERROR. It doesn't know to ignore the first 0 yet.
-

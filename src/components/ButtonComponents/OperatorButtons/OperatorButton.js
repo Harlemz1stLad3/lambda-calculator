accda259f055f1f8197bd56ae623e008a84b8a2b
Juanita Purcell <brittanyhugz@gmail.com>
2019-08-27 10:46:15 -0400

    committing uodates

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,17 +1,11 @@
 import React from "react";
 
-const OperatorButton = ({ setDisplay, value, symbol, display }) => {
-
-  function handleOperator() {
-    if (symbol === '+' || '-' || 'x' || '/' ) return setDisplay(display + value);
-  }
-
-  function handleCalculation() {
-    if (value === '=') return setDisplay(eval(display));
-  }
-
+const OperatorButton = (props) => {
+  console.log(props)
   return (
-    <div className="operator-button" value={value} onClick={() => handleOperator() || handleCalculation()}>{ symbol }</div>
+    <button onClick = {() => props.addOperator(props.operator.value)}>
+      {props.operator.char}
+    </button>
   );
 };
 
